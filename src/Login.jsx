503c523bc2efc25cc169ffcdfc66e1deb70b0244
sanasham <sayyedesubvali@gmail.com>
2025-08-09 11:26:50 +0530

    login form added

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,48 @@
+import { useState } from 'react';
+
 const Login = () => {
-  return <div>Login Page</div>;
+  const [emailId, setEmailId] = useState('');
+  const [password, setPassword] = useState('');
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    // Handle login logic here
+    console.log('Email:', emailId, 'Password:', password);
+  };
+  return (
+    <div className='flex justify-center items-center my-10'>
+      <div className='card card-border bg-base-200 w-96 '>
+        <div className='card-body'>
+          <h2 className='card-title justify-center'>Login</h2>
+          <div>
+            <fieldset className='fieldset'>
+              <legend className='fieldset-legend'>Email ID</legend>
+              <input
+                type='text'
+                className='input'
+                value={emailId}
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+              <legend className='fieldset-legend'>Password</legend>
+              <input
+                type='password'
+                className='input'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </fieldset>
+          </div>
+          <div className='card-actions justify-center'>
+            <button
+              className='btn btn-primary bg-amber-300 '
+              onClick={handleLogin}
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
